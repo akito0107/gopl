ea5f033aca156cf28a2eeff96328e4c71b01304a
Akito Ito <akito0107@gmail.com>
2017-11-23 10:18:46 +0900

    add ch02

---
+++ ch02/tempconv/conv.go
@@ -0,0 +1,25 @@
+package tempconv
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k + 273.15)
+}
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - 273.15)
+}
+
+func FToK(f Fahrenheit) Kelvin {
+	return CToK(FToC(f))
+}
+
+func KToF(k Kelvin) Fahrenheit {
+	return CToF(KToC(k))
+}
