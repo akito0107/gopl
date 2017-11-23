dc689190df5dd61a76e2deb9e051af2d0977c5ec
Akito Ito <akito0107@gmail.com>
2017-11-23 16:54:55 +0900

    update

--- ch02/ex04/popcount_shift.go
+++ ch02/ex04/popcount_shift.go
@@ -1,18 +1,14 @@
 package ex03
 
-var pc [256]byte
-
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
 func PopCountShift(x uint64) int {
 	s := 1 & x
 	var i uint64
 	for i = 1; i < 64; i++ {
-		s += 1 & (x >> i)
+		xi := x >> i
+		if xi == 0 {
+			break
+		}
+		s += 1 & xi
 	}
 	return int(s)
 }
--- ch02/ex05/popcount_lowest.go
+++ ch02/ex05/popcount_lowest.go
@@ -1,13 +1,5 @@
 package ex03
 
-var pc [256]byte
-
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
 func PopCountLowest(x uint64) int {
 	s := 0
 	for x != 0 {
