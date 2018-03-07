ebdb7ad4b26b0e0816fbc1d657ba4386db879c7b
Akito Ito <akito0107@gmail.com>
2018-03-08 00:19:25 +0900

    solve 10/01

---
+++ ch10/ex01/jpeg.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"os"
+)
+
+var outFormat = flag.String("f", "png", "output format (default png)")
+
+func main() {
+	flag.Parse()
+	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "input format = ", kind)
+	fmt.Fprintln(os.Stderr, "output format = ", *outFormat)
+
+	switch *outFormat {
+	case "jpeg":
+		return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+	case "png":
+		return png.Encode(out, img)
+	case "gif":
+		return gif.Encode(out, img, &gif.Options{})
+	default:
+		return errors.New("unsupported output format")
+	}
+}
---
+++ ch10/jpeg.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
+	"os"
+)
+
+func main() {
+	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "input format = ", kind)
+	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+}
