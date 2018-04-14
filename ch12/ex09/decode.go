1ffd23fff4846d04242bbcbcaf522a1dfdd778e9
Akito Ito <akito0107@gmail.com>
2018-04-15 01:01:39 +0900

    update

---
+++ ch12/ex09/decode.go
@@ -0,0 +1,67 @@
+package sexpr
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"text/scanner"
+)
+
+type Decoder struct {
+	lex *lexer
+	r   io.Reader
+}
+
+type Symbol struct {
+	Name string
+}
+
+type String struct {
+	Val string
+}
+
+type Int struct {
+	Val int
+}
+
+type StartList struct{}
+type EndList struct{}
+
+func NewDecoder(r io.Reader) *Decoder {
+	lex := lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(r)
+	return &Decoder{lex: &lex, r: r}
+}
+
+func (d *Decoder) Decode(v interface{}) (err error) {
+	d.lex.next()
+	defer func() {
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", d.lex.scan.Position, x)
+		}
+	}()
+	read(d.lex, reflect.ValueOf(v).Elem())
+	return nil
+}
+
+func (d *Decoder) Token() (interface{}, error) {
+	d.lex.next()
+	switch d.lex.token {
+	case scanner.Ident:
+		return &Symbol{d.lex.text()}, nil
+	case scanner.String:
+		s, _ := strconv.Unquote(d.lex.text())
+		return &String{s}, nil
+	case scanner.Int:
+		i, _ := strconv.Atoi(d.lex.text())
+		return &Int{i}, nil
+	case '(':
+		return &StartList{}, nil
+	case ')':
+		return &EndList{}, nil
+	case scanner.EOF:
+		return nil, io.EOF
+	}
+	panic(fmt.Sprintf("unexpected token %q", d.lex.text()))
+}
---
+++ ch12/ex09/parser.go
@@ -0,0 +1,98 @@
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"strconv"
+	"text/scanner"
+)
+
+func Unmarshal(data []byte, out interface{}) (err error) {
+	lex := &lexer{scan: scanner.Scanner{Mode: scanner.GoTokens}}
+	lex.scan.Init(bytes.NewReader(data))
+	lex.next()
+	defer func() {
+		if x := recover(); x != nil {
+			err = fmt.Errorf("error at %s: %v", lex.scan.Position, x)
+		}
+	}()
+	read(lex, reflect.ValueOf(out).Elem())
+	return nil
+}
+
+func read(lex *lexer, v reflect.Value) {
+	switch lex.token {
+	case scanner.Ident:
+		if lex.text() == "nil" {
+			v.Set(reflect.Zero(v.Type()))
+			lex.next()
+			return
+		}
+	case scanner.String:
+		s, _ := strconv.Unquote(lex.text())
+		v.SetString(s)
+		lex.next()
+		return
+	case scanner.Int:
+		i, _ := strconv.Atoi(lex.text())
+		v.SetInt(int64(i))
+		lex.next()
+		return
+	case '(':
+		lex.next()
+		readList(lex, v)
+		lex.next()
+		return
+	}
+	panic(fmt.Sprintf("unexpected token %q", lex.text()))
+}
+
+func readList(lex *lexer, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Array:
+		for i := 0; !endList(lex); i++ {
+			read(lex, v.Index(i))
+		}
+	case reflect.Slice:
+		for !endList(lex) {
+			item := reflect.New(v.Type().Elem()).Elem()
+			read(lex, item)
+			v.Set(reflect.Append(v, item))
+		}
+	case reflect.Struct:
+		for !endList(lex) {
+			lex.consume('(')
+			if lex.token != scanner.Ident {
+				panic(fmt.Sprintf("got token %q, want filed name", lex.text()))
+			}
+			name := lex.text()
+			lex.next()
+			read(lex, v.FieldByName(name))
+			lex.consume(')')
+		}
+	case reflect.Map:
+		v.Set(reflect.MakeMap(v.Type()))
+		for !endList(lex) {
+			lex.consume('(')
+			key := reflect.New(v.Type().Key()).Elem()
+			read(lex, key)
+			value := reflect.New(v.Type().Elem()).Elem()
+			read(lex, value)
+			v.SetMapIndex(key, value)
+			lex.consume(')')
+		}
+	default:
+		panic(fmt.Sprintf("cannot decode list int %v", v.Type()))
+	}
+}
+
+func endList(lex *lexer) bool {
+	switch lex.token {
+	case scanner.EOF:
+		panic("end of file")
+	case ')':
+		return true
+	}
+	return false
+}
